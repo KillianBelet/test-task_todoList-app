95e129d2e70ab5f53569a9c48ca87a9305ad2860
Killian <killian.delettre@gmail.com>
2025-05-22 18:14:09 +0200

    connection du frontend déployé au backend déployé

--- todo-app-frontend/src/app/services/task.service.ts
+++ todo-app-frontend/src/app/services/task.service.ts
@@ -1,28 +1,30 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {Task} from "../models/task";
-import {API_URL} from "../app.component";
 import { Observable } from "rxjs";
+import { environment } from "../../environments/environment.prod";
 
 @Injectable({ providedIn: 'root' })
 export class TaskService {
 
+    private baseUrl = environment.apiBase;  
+
   constructor(private http: HttpClient) {}
 
   getTasks() {
-    return this.http.get<Task[]>(API_URL + '/tasks');
+    return this.http.get<Task[]>(this.baseUrl + '/tasks');
   }
 
   createTask(title: string, deadline: string) {
-    return this.http.post<Task>(API_URL + '/task', { title, deadline });
+    return this.http.post<Task>(this.baseUrl + '/task', { title, deadline });
   }
 
 
   updateTask(id: string, patch: Partial<Pick<Task,'completed'|'favorite'>>): Observable<Task> {
-    return this.http.patch<Task>(`${API_URL}/task/${id}`, patch);
+    return this.http.patch<Task>(`${this.baseUrl}/task/${id}`, patch);
   }
 
   deleteTask(id: string) {
-    return this.http.delete(`${API_URL}/task/${id}`);
+    return this.http.delete(`${this.baseUrl}/task/${id}`);
   }
 }
