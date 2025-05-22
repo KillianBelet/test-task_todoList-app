c7de0e027865fd38e0961de19a47a5632a6f92ef
Killian <killian.delettre@gmail.com>
2025-05-22 17:14:43 +0200

    modification variable d'environnement pour une mise en production

--- todo-app-backend/config/datastores.js
+++ todo-app-backend/config/datastores.js
@@ -31,31 +31,12 @@ module.exports.datastores = {
   * (For production configuration, see `config/env/production.js`.)          *
   *                                                                          *
   ***************************************************************************/
-
   default: {
-
-    /***************************************************************************
-    *                                                                          *
-    * Want to use a different database during development?                     *
-    *                                                                          *
-    * 1. Choose an adapter:                                                    *
-    *    https://sailsjs.com/plugins/databases                                 *
-    *                                                                          *
-    * 2. Install it as a dependency of your Sails app.                         *
-    *    (For example:  npm install sails-mysql --save)                        *
-    *                                                                          *
-    * 3. Then pass it in, along with a connection URL.                         *
-    *    (See https://sailsjs.com/config/datastores for help.)                 *
-    *                                                                          *
-    ***************************************************************************/
     adapter: 'sails-mongo',
 
-    // En dev : sails.config.mongo.uri (depuis local.js)
-    // En prod : process.env.MONGODB_URI
-    url: (process.env.NODE_ENV === 'production')
-      ? process.env.MONGODB_URI
-      : sails.config.mongo.uri
-    // replace USERNAME, PASSWORD (given by email) and DBNAME (test-task-[YOUR-NAME])
+
+    url: process.env.MONGO_URI || sails.config.mongo.uri,
+
 
   },
 
